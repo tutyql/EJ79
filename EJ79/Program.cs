4f4882caf46cc65237fe97af31e6c3071c9a5e3a
Euge <Euge@DESKTOP-S716L4F>
2022-11-18 01:01:39 -0300

    Agregar archivos de proyecto.

---
+++ EJ79/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Frase;
+
+            SortedSet<string> datos = new SortedSet<string>();
+
+            Console.WriteLine("Ingrese una frase");
+            Frase = Console.ReadLine();
+
+            while (Frase != " ")
+            {
+                if (datos.Contains(Frase))
+                {
+                    Console.WriteLine("\nESTA FRASE YA EXISTE\n---------------\n");
+                }
+                datos.Add(Frase);
+                Console.WriteLine("\nIngrese siguente frase\n");
+                Frase = Console.ReadLine();
+            }
+
+            
+
+        }
+    }
+}
